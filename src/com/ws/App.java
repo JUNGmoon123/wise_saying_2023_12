97dfe090e770bbe120fa5d6ab6634f7b960cea01
JUNGmoon123 <jungpro567@gmail.com>
2023-12-21 10:02:49 +0900

    유지보수 및 협업을 위해 모듈 분리, 패키지 이동

--- src/com/ws/App.java
+++ src/com/ws/App.java
@@ -1,6 +1,10 @@
+package com.ws;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import com.ws.wiseSaying.entity.WiseSaying;
 
 public class App {
 	private Scanner sc;
@@ -39,7 +43,7 @@ public class App {
 					System.out.println("등록 된 명언이 없어");
 				} else {
 					System.out.println("번호  /  작가  /  명언  ");
-					System.out.println("=".repeat(30));
+					System.out.println("=".repeat(30));	//=를 30번 반복하겠다는 의미.
 					//역순으로 출력하기 위해 최대값에서 -1을 해줘야한다.
 					//-1을 안하면 인덱스의 마지막번호는 크기-1 이기 때문이다.
 					for (int i = wiseSayings.size() - 1; i >= 0; i--) {
---
+++ src/com/ws/system/controller/SystemController.java
@@ -0,0 +1,5 @@
+package com.ws.system.controller;	//폴더를 나누는것, 정리하는것, 의미.
+
+public class SystemController {
+
+}
